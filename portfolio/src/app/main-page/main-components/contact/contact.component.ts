94e519b8ff869dccd5bce16a08aec7ffec663955
Gosia <lm.sprachen@gmail.com>
2024-08-02 11:52:07 +0200

    portfolio

--- portfolio/src/app/main-page/main-components/contact/contact.component.ts
+++ portfolio/src/app/main-page/main-components/contact/contact.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { InfoComponent } from './info/info.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [InfoComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
--- portfolio/src/app/main-page/main-components/portfolio/portfolio.component.ts
+++ portfolio/src/app/main-page/main-components/portfolio/portfolio.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
-export class PortfolioComponent {
 
+export class PortfolioComponent implements OnInit {
+  projects: any[];  // Zdefiniuj właściwość 'projects'
+
+  constructor() {
+    // Inicjalizacja 'projects' może nastąpić tutaj lub w ngOnInit
+    this.projects = [
+      { id: 1, name: 'Project 1', description: 'Description 1', img1: 'Join.png', img2: 'pollo_loco.png' },
+      
+    ];
+  }
+
+  ngOnInit(): void {
+    // Możesz również zainicjalizować 'projects' tutaj, jeśli potrzebujesz
+  }
 }
