e29870213ec8d858b941ebbb3bd21894610db3e9
Gosia <lm.sprachen@gmail.com>
2024-08-02 12:35:00 +0200

    logo

--- portfolio/src/app/main-page/main-components/portfolio/portfolio.component.ts
+++ portfolio/src/app/main-page/main-components/portfolio/portfolio.component.ts
@@ -1,4 +1,4 @@
-import { CommonModule } from '@angular/common';
+/*import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -10,13 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class PortfolioComponent implements OnInit {
+  projects: any[]; 
+
+  constructor() {
+    this.projects = [
+      { id: 1, name: 'Join', description: 'Angular|TypeScript|HTML|CSS|Friebase', img1: 'Join.png', img2: 'pollo_loco.png' },
+      
+    ];
+  }
+
+  ngOnInit(): void {
+    
+  }
+}*/
+
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './portfolio.component.html',
+  styleUrls: ['./portfolio.component.scss']
+})
+export class PortfolioComponent implements OnInit {
   projects: any[];  // Zdefiniuj właściwość 'projects'
 
   constructor() {
     // Inicjalizacja 'projects' może nastąpić tutaj lub w ngOnInit
     this.projects = [
-      { id: 1, name: 'Project 1', description: 'Description 1', img1: 'Join.png', img2: 'pollo_loco.png' },
-      
+      {
+        id: 1,
+        name: 'Join',
+        description: 'Angular|TypeScript|HTML|CSS|Friebase',
+        img1: 'Join.png',
+        img2: 'pollo_loco.png',
+        img2Name:'El Pollo Loco',
+        img2Description: 'HTML|CSS|Javascript',
+        link: 'https://live-link-example.com',
+        git: 'https://github.com/example'
+      },
     ];
   }
 
