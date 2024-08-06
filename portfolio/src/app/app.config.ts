b4d3adc7f8a1c02e5613b86090c338f10459516f
Gosia <lm.sprachen@gmail.com>
2024-08-06 15:01:51 +0200

    animationen

--- portfolio/src/app/app.config.ts
+++ portfolio/src/app/app.config.ts
@@ -2,10 +2,28 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+// Poprawiono nazwę funkcji, aby uniknąć konfliktu nazw
+export function createTranslateHttpLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json'); // Poprawiono brakujący przecinek
+}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(),
+    TranslateService,
+    TranslateModule.forRoot({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateHttpLoader, // Użyto poprawionej funkcji
+        deps: [HttpClient]
+      }
+    }).providers!
+  ],
 };
-
-
 
